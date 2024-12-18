78685607cf47125f694599adb5ce35ecb9f89c09
Vijay Subramanian <47090672+baranidar@users.noreply.github.com>
2024-12-18 10:50:56 -0800

    Update selection_sort_recursive.py

--- selection_sort_recursive.py
+++ selection_sort_recursive.py
@@ -1,22 +1,27 @@
 def selection_sort(lst, length = 0):
-  print("Start",lst, length)
-  if length < 1:
+  if length == 0:
+      length = len(lst)
+  if length == 0:
       return lst
-  
+  print(lst, length)
   min_val = lst[len(lst) - length]
   min_index = len(lst) - length
-  print(min_val, min_index)
+  #print(min_val, min_index)
   for index,item in enumerate(lst):
-      print(index, item, min_index)
+      #print(index, item, min_index)
       if index > min_index:
           if lst[index] < min_val:
-              print("-->", lst[index], min_val)
+              #print("-->", lst[index], min_val)
               min_val = lst[index]
               min_index = index
   tmp = lst[len(lst) - length]
   lst[len(lst) - length] = min_val
   lst[min_index] = tmp
-  return selection_sort(lst, length-1)
+  if length-1 > 0:
+      return selection_sort(lst, length-1) 
+  else:
+      return lst
   
-      
-      
+lst = [29, 10, 14, 37, 13]
+output = selection_sort(lst)
+print(output)
